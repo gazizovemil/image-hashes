fe848e9b31ac30da5d680afd5a4e638ac22779b1
Daniel Barnes <dabarnes2b@gmail.com>
2021-10-16 10:58:00 -0700

    lint cleanup?

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
-from flask import request
-from flask.wrappers import Request
 import hashlib
 
+from flask import Flask
+from flask import request
+
 app = Flask(__name__)
+
 
 @app.route("/up")
 def upCheck():
@@ -11,21 +12,11 @@ def upCheck():
 
 @app.route("/image", methods=["POST"] )
 def image():
-    print(request)
     data = request.get_data()
-    #print(temp)
-    #fileKeys = request.files.keys()
-    #if len(fileKeys) > 1 :
-    #    print("expected only one item")
-
-
-    #print(request.files)
-    ##data = request.files.popitem()
-
     hashObjs = {
         "sha1": hashlib.sha1(),
         "sha256": hashlib.sha256(),
-        "md5": hashlib.md5()
+        "md5": hashlib.md5(),
     }
     for _, val in hashObjs.items():
         val.update(data)
@@ -33,5 +24,5 @@ def image():
     return {
         "sha1": hashObjs["sha1"].hexdigest(),
         "sha256": hashObjs["sha256"].hexdigest(),
-        "md5": hashObjs["md5"].hexdigest()
+        "md5": hashObjs["md5"].hexdigest(),
     }
